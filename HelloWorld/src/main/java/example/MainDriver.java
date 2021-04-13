768fe46955d8b280df71c2722deba92c033dbf57
TChester <trevin.chester@gmail.com>
2021-04-13 08:04:13 -0400

    first push

---
+++ HelloWorld/src/main/java/example/MainDriver.java
@@ -0,0 +1,8 @@
+package example;
+
+public class MainDriver {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Prepare yourself for battle...");
+    }
+}
